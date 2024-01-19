a15f06a35070405f18068895c43d46f32c2fc7d8
Anshuman Garg <68477154+Anshuman1303@users.noreply.github.com>
2024-01-19 17:40:52 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,22 @@
 create database gamedb;
 use gamedb;
-create table userdb(username varchar(20) primary key, password varchar(100) not null, money float not null, admin varchar(1) default 'F', banned date default NULL);
-create table usermsgs(sender varchar(20) not null,recipient varchar(20) not null, message varchar(1024) not null, IsRead varchar(1) default 'F', MsgTimeStamp datetime default current_timestamp, foreign key (sender) references userdb (username),foreign key (recipient) references userdb (username));
+CREATE TABLE `userdb` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `money` float NOT NULL,
+  `admin` varchar(1) DEFAULT 'F',
+  `banned` date DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `usermsgs` (
+  `sender` varchar(20) NOT NULL,
+  `recipient` varchar(20) NOT NULL,
+  `message` varchar(1024) NOT NULL,
+  `IsRead` varchar(1) DEFAULT 'F',
+  `MsgTimeStamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  KEY `sender` (`sender`),
+  KEY `recipient` (`recipient`),
+  CONSTRAINT `usermsgs_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `userdb` (`username`),
+  CONSTRAINT `usermsgs_ibfk_2` FOREIGN KEY (`recipient`) REFERENCES `userdb` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 insert into userdb values('admin','admin123',1.175494351E+38,'T',null);
